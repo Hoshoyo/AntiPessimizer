72232d32c11a7b94fc0dd3715ad7d6f4c8593567
Pedro Sassen Veiga <psveiga@nelogica.com.br>
2024-04-24 12:36:46 -0300

    Remove executable from repo

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 
-pushd Win64\Debug
-call cl /nologo /I../../include /I../../src/imgui /Zi ../../src/*.cpp ../../src/*.c ../../src/imgui/*.cpp /Fe:AntiPessimizer.exe /link kernel32.lib d3d11.lib Comdlg32.lib
+pushd Win64\Release
+rem call cl /nologo /I../../include /I../../src/imgui /Zi ../../src/*.cpp ../../src/*.c ../../src/imgui/*.cpp /Fe:AntiPessimizer.exe /link kernel32.lib d3d11.lib Comdlg32.lib winmm.lib shell32.lib
+call cl /O2 /nologo /I../../include /I../../src/imgui /Zi ../../src/*.cpp ../../src/*.c ../../src/imgui/*.cpp /Fe:AntiPessimizer.exe /link kernel32.lib d3d11.lib Comdlg32.lib winmm.lib shell32.lib
+
 popd
